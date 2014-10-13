b576380f27f9376a19c9b4e32c30d4baf5e49dc6
Anthony Ho <ahomnky11@gmail.com>
2014-10-13 11:50:42 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Odot::Application.config.secret_key_base = '4edc28e7865882aef572dd01c4a32bb8493da4f6e5349ea7f2b2fc58c2076f9450aa7e780cfd7f8eb8e9de3499da4f2d5aa8b4e789eadf6b60001067a904cfbb'
