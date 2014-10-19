d1d8aaa7248e119fc27fcd85db416a39b98a3b11
Anthony Ho <ahomnky11@gmail.com>
2014-10-19 20:56:37 +0100

    Add length and presence validation

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -7,7 +7,7 @@ describe "Creating todo lists" do
        expect(page).to have_content("New todo_list")
        
     fill_in "Title", with: "My todo list"
-     fill_in "Description", with: "This is what I'm doing today."
+     fill_in "Description", with: "This is what I'm doing today"
     click_button "Create Todo list"
        
     expect(page).to have_content("My todo list")
@@ -15,22 +15,39 @@ describe "Creating todo lists" do
     
     it "displays an error when the todo list has no title" do
         
-    expect(Todolist.count).to eq(0)
+    expect(TodoList.count).to eq(0)
         
-        visit "/todo_lists"
+    visit "/todo_lists"
        click_link "New Todo list"
        expect(page).to have_content("New todo_list")
        
     fill_in "Title", with: ""
-    fill_in "Description", with: "This is what I'm doing today."
+    fill_in "Description", with: "This is what I'm doing today"
     click_button "Create Todo list"
     expect(page).to have_content("error")
     expect(TodoList.count).to eq(0)
         
     visit "/todo_lists"
-    expect(page).to_not have_content("This is what I'm doing today.")
-        end
+    expect(page).to_not have_content("This is what I'm doing today")
+end
     
+    it "displays an error when the todo list has a title less than three characters" do
+        
+    expect(TodoList.count).to eq(0)
+        
+    visit "/todo_lists"
+       click_link "New Todo list"
+       expect(page).to have_content("New todo_list")
+       
+    fill_in "Title", with: "Hi"
+    fill_in "Description", with: "This is what I'm doing today"
+    click_button "Create Todo list"
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+        
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is what I'm doing today")
+        end
 end
 
     
